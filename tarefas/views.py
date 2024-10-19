e1140896e6f7fa65f732abb120763d325c222c30
NathanOnCodes <contatonathancf@gmail.com>
2024-10-19 16:46:26 -0300

    feat: views att

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
+from .models import ListaTarefa
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.user.is_authenticated:
+        return redirect('home/')
+    return redirect('login/')
+
+def home(request):
+    tarefas = ListaTarefa.objects.all().order_by('-id')
+    pagination = Paginator(tarefas, 5)
+    
+    context = {
+        'posts': pagination.page(1)
+    }
+    return render(request, 'index.html', context)
+
+
+def get_login(request):
+    return render(request, 'login.html')
