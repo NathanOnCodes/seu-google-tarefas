73f5579871d77c61475929c8c38538284e39d56a
NathanOnCodes <contatonathancf@gmail.com>
2024-10-19 16:45:20 -0300

    feat: urls att

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -1,7 +1,8 @@
-from .views import index
+from .views import index, get_login
 from django.urls import path
 
 urlpatterns = [
     path('', index, name='index'),
-    #path('tarefas/', tarefas, name='tarefas')
+    path('login/', get_login, name='login'),
+    path('home/', index, name='home'),
 ]
