12f204731e34d02423a1e7d0c5208f019c88b930
NathanOnCodes <contatonathancf@gmail.com>
2024-10-19 16:32:18 -0300

    feat: name admin page

--- tarefas/apps.py
+++ tarefas/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class TarefasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tarefas'
+    verbose_name = "Area de tarefas"
+    verbose_name_plural = "Areas de tarefas"
