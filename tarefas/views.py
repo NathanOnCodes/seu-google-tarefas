0670c0f3f7507888ea63d0cdc7517dcbb75dbd96
NathanOnCodes <contatonathancf@gmail.com>
2024-10-29 00:21:42 -0300

    refactor: criar tarefa and home refact

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.core.paginator import Paginator
+from django.urls import include
 from .models import ListaTarefa
+from .forms import ListaTarefaForm
 
 # Create your views here.
-def index(request):
-    if request.user.is_authenticated:
-        return redirect('home/')
-    return redirect('contas/')
+
 
 def home(request):
     tarefas = ListaTarefa.objects.all().order_by('-id')
@@ -15,6 +14,14 @@ def home(request):
     context = {
         'posts': pagination.page(1)
     }
-    return render(request, 'index.html', context)
+    return render(request, 'home.html', context)
 
+
+
+def criar_tarefa(request):
+    form = ListaTarefaForm()
+    context = {
+        "form": form
+    }
+    return render(request, 'criar_tarefa.html', context)
 
