96b8fb9bee49be4b0c5e30d2c70fac52a8fcf43c
NathanOnCodes <contatonathancf@gmail.com>
2024-10-29 00:19:43 -0300

    refactor: form model lista tarefas

--- tarefas/forms.py
+++ tarefas/forms.py
@@ -1,40 +1,8 @@
 from django import forms
-from django.utils.text import slugify
-from .models import ListaTarefa, Tarefa
+from .models import ListaTarefa
 
-class TarefaCriandoForm(forms.ModelForm):
-    slug = forms.CharField(required=False, widget=forms.widgets.HiddenInput())
+
+class ListaTarefaForm(forms.ModelForm):
     class meta:
         model = ListaTarefa
-        fields = ("nome", "slug")
-
-    def limpar_nome(self) -> str:
-        nome: str = self.cleaned_data.get("nome")
-        slug = slugify(nome)
-        if ListaTarefa.objects.filter(slug=slug).exists():
-            raise forms.ValidationError(f"A Lista de tarefas {nome} ja existe")
-        return nome
-    
-    def salvar(self, commit: bool = True) -> ListaTarefa: 
-        lista_tarefa: ListaTarefa = super().save(commit)
-        lista_tarefa.slug = slugify(lista_tarefa.nome)
-        lista_tarefa.save()
-        return lista_tarefa
-    
-
-class TarefaForm(forms.ModelForm):
-    class Meta:
-        model = Tarefa
-        fields = ("nome", "concluida")
-
-    def limpar_nome(self) -> str:
-        nome: str = self.cleaned_data["nome"]
-        if Tarefa.objects.filter(nome=nome).exclude(concluida=True).exists():
-            raise forms.ValidationError(f"A tarefa {nome} ja existe")
-        return nome
-    
-    
-    def salvar(self, commit: bool = True) -> Tarefa:
-        tarefa: Tarefa = super().save(commit)
-        tarefa.save()
-        return tarefa
+        fields = ["nome", "slug", "tarefas"]
