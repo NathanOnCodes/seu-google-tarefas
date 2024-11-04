36113e38c5e8cda3d7f4e8e4b5c7471a9b7c82dc
NathanOnCodes <contatonathancf@gmail.com>
2024-11-04 15:56:12 -0300

    feat: criar tarefa & tarefas endpoint

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -1,7 +1,8 @@
-from .views import home
+from .views import home, criar_tarefa
 from django.urls import path, include
 
 urlpatterns = [
     path('', home, name='home'),
     path('contas/', include('allauth.urls'), name='contas'),
+    path('tarefa/criar', criar_tarefa, name='criar-tarefa'),
 ]
