e8e2a5046ce0caffdab574bc172cc173128bb2bd
NathanOnCodes <contatonathancf@gmail.com>
2024-11-04 15:59:03 -0300

    fix: refactor name view tarefaS

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -1,8 +1,9 @@
-from .views import home, criar_tarefa
+from .views import home, criar_tarefa, tarefas
 from django.urls import path, include
 
 urlpatterns = [
     path('', home, name='home'),
     path('contas/', include('allauth.urls'), name='contas'),
-    path('tarefa/criar', criar_tarefa, name='criar-tarefa'),
+    path('tarefas/', tarefas, name='tarefas'),
+    path('tarefas/criar', criar_tarefa, name='criar-tarefa'),
 ]
