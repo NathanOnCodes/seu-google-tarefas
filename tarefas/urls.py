3a172fde2262317f4f489b466d0589de024d4081
NathanOnCodes <contatonathancf@gmail.com>
2024-11-08 19:22:19 -0300

    feat: add new endpoints

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('contas/', include('allauth.urls'), name='contas'),
     path('tarefas/criar/', views.criar_lista_tarefa, name='criar-lista'),
     path('tarefas/', views.lista_tarefas, name='lista-tarefas'),
+    path('tarefas/<slug:slug>/atualizar/', views.atualizar_lista, name='atualizar-lista'),
+    path('tarefas/<slug:slug>/deletar/', views.deletar_lista, name='deletar-lista'),
+    path('tarefas/<slug:slug>/tarefa/<int:tarefa_id>/deletar/', views.deletar_tarefa, name='deletar-tarefa'),
+    path('tarefas/<slug:slug>/tarefa/<int:tarefa_id>/atualizar/', views.atualizar_tarefa, name='atualizar-tarefa'),
 ]
