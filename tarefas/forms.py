e1e66e4806587651c09ce72955996b3b1e365bd8
NathanOnCodes <contatonathancf@gmail.com>
2024-11-07 22:50:31 -0300

    refactor: new ListaTarefasForm from scratch

--- tarefas/forms.py
+++ tarefas/forms.py
@@ -1,8 +1,20 @@
 from django import forms
 from .models import ListaTarefa
 
-
 class ListaTarefaForm(forms.ModelForm):
-    class meta:
+    nome_lista = forms.CharField(
+        label="Nome da lista",
+        widget=forms.TextInput(attrs={'class':'form-control'})
+    )
+    tarefas = forms.CharField(
+        label='Tarefas',
+        widget=forms.Textarea(attrs={
+            'class':'form-control',
+            'placeholder':'Adicione uma tarefa',
+        }),
+        help_text='Digite uma tarefa por linha.',
+    )
+
+    class Meta:
         model = ListaTarefa
-        fields = ["nome", "slug", "tarefas"]
+        fields = ['nome_lista']
