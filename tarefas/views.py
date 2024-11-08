5eee8bbc1bb6111882223fb6c2bc9d4cda248555
NathanOnCodes <contatonathancf@gmail.com>
2024-11-08 19:04:40 -0300

    feat: add atualizar_lista view

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
 from django.utils.text import slugify
 from .models import ListaTarefa, Tarefa
@@ -34,11 +34,36 @@ def criar_lista_tarefa(request):
                     nova_tarefa = Tarefa.objects.create(nome=texto.strip())
                     lista.tarefas.add(nova_tarefa)
 
-            return redirect('todo:lista_tarefas')
+            return redirect('todo:lista-tarefas')
     else:
         form = ListaTarefaForm()
     return render(request, 'form.html', {'form': form})
-            
+
+
+
+def atualizar_lista(request, slug):
+    lista = get_object_or_404(ListaTarefa, slug=slug)
+    if request.method == 'POST':
+        form = ListaTarefaForm(request.POST)
+        if form.is_valid():
+            lista.nome = form.cleaned_data['nome_lista']
+            lista.slug = slugify(form.cleaned_data['nome_lista'])
+            lista.save()
+
+            tarefas_texto = form.cleaned_data['nome_tarefa'].split('\n')
+
+            for texto in tarefas_texto:
+                if texto.strip():
+                    nova_tarefa = Tarefa.objects.create(nome=texto.strip())
+                    lista.tarefas.add(nova_tarefa)
+
+            return redirect('todo:lista-tarefas')
+    else:
+        form = ListaTarefaForm(initial={
+            'nome_lista': lista.nome,
+            'nome_tarefa': '\n'.join([tarefa.nome for tarefa in lista.tarefas.all()])
+        })
+    return render(request, 'atualizar_lista.html', {'form': form, 'lista': lista})     
 
 def lista_tarefas(request):
     listas = ListaTarefa.objects.all()
