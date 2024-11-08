42f733d1e4b766cd95e080fabe2d077d58f6e05c
NathanOnCodes <contatonathancf@gmail.com>
2024-11-07 22:52:49 -0300

    feat: parse urls and new adopt name

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -1,11 +1,10 @@
-from .views import home, criar_tarefa, tarefas
+from . import views
 from django.urls import path, include
 
-
-app_name = 'tarefas'
+app_name = 'todo'
 urlpatterns = [
-    path('', home, name='home'),
+    path('', views.home, name='home'),
     path('contas/', include('allauth.urls'), name='contas'),
-    path('cadastrar/', tarefas, name='tarefas'),
-    path('cadastrar/criar/', criar_tarefa, name='criar-tarefa'),
+    path('tarefas/criar/', views.criar_lista_tarefa, name='criar-lista'),
+    path('tarefas/', views.lista_tarefas, name='lista-tarefas'),
 ]
