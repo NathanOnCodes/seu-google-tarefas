4e24103354e48f47f8542fbe31e5df928c64765b
NathanOnCodes <contatonathancf@gmail.com>
2024-11-08 17:40:27 -0300

    fix: form lista resolve

--- tarefas/forms.py
+++ tarefas/forms.py
@@ -3,16 +3,16 @@ from .models import ListaTarefa
 
 class ListaTarefaForm(forms.ModelForm):
     nome_lista = forms.CharField(
-        label="Nome da lista",
+        label="Nome da lista:",
         widget=forms.TextInput(attrs={'class':'form-control'})
     )
-    tarefas = forms.CharField(
-        label='Tarefas',
+    nome_tarefa = forms.CharField(
+        label='Nome da tarefa:',
         widget=forms.Textarea(attrs={
-            'class':'form-control',
-            'placeholder':'Adicione uma tarefa',
+            'class': 'form-control',
+            'placeholder': 'Digite uma tarefa por linha'
         }),
-        help_text='Digite uma tarefa por linha.',
+        help_text='Digite uma tarefa por linha'
     )
 
     class Meta:
