e9b46025a0e3e974764f679d5fe93cc4ce8db897
NathanOnCodes <contatonathancf@gmail.com>
2024-11-12 19:56:09 -0300

    feat: add deletar lista test

--- tarefas/tests/test_views.py
+++ tarefas/tests/test_views.py
@@ -1,5 +1,5 @@
 import pytest
-from tarefas.models import ListaTarefa
+from tarefas.models import ListaTarefa, Tarefa
 from django.urls import reverse
 from django.test import Client
 
@@ -24,22 +24,53 @@ def test_criar_lista_tarefa_view():
 @pytest.mark.django_db
 def test_atualizar_lista_view():
     client = Client()
-    form_data = {
-        'nome_lista': 'Nova Lista de Tarefas',
-        'nome_tarefa': 'Tarefa 1\nTarefa 2\nTarefa 3'
-    }
 
-    response = client.post(reverse('criar-lista'), form_data)
-    lista = ListaTarefa.objects.get(nome='Nova Lista de Tarefas')
+    lista = ListaTarefa.objects.create(
+        nome="Lista de teste",
+        slug="lista-de-teste"
+    )
+ 
+    lista.tarefas.add(Tarefa.objects.create(nome="Tarefa 1"))
+    lista.tarefas.add(Tarefa.objects.create(nome="Tarefa 2"))
+    lista.tarefas.add(Tarefa.objects.create(nome="Tarefa 3"))
+
     data_att = {
-        'nome_lista': 'Nova Lista de Tarefas',
+        'nome_lista': 'Lista de teste',
         'nome_tarefa': 'Tarefa 4\nTarefa 5\nTarefa 6'
     }
-   
-    response = client.post(reverse('atualizar-lista', args=[lista.slug]), data=data_att)
+
+    
+    response = client.post(reverse('atualizar-lista', kwargs={
+        'slug': lista.slug,
+    }), data=data_att)
+
     assert response.status_code == 302
 
     tarefas = lista.tarefas.all()
     assert tarefas.get(nome='Tarefa 4') is not None
     assert tarefas.get(nome='Tarefa 5') is not None
     assert tarefas.get(nome='Tarefa 6') is not None
+
+
+@pytest.mark.django_db
+def test_deletar_tarefa_view():
+    client = Client()
+
+    lista = ListaTarefa.objects.create(
+        nome="Nova Lista de Tarefas",
+        slug="nova-lista-de-tarefas"
+    )    
+
+    tarefa1 = Tarefa.objects.create(nome="Tarefa 1")
+    tarefa2 = Tarefa.objects.create(nome="Tarefa 2")
+    lista.tarefas.add(tarefa1, tarefa2)
+
+    assert ListaTarefa.objects.count() == 1
+
+    response = client.post(
+        reverse('deletar-lista', kwargs={"slug": lista.slug}),
+    )
+
+    assert response.status_code == 302
+
+    assert ListaTarefa.objects.count() == 0
