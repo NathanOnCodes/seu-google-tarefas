0a10e98db5e1d723ff2fa20ea7b1389f6ac1287a
NathanOnCodes <contatonathancf@gmail.com>
2024-11-24 21:10:15 -0300

    chore: add static root

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -1,5 +1,7 @@
 from . import views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -10,4 +12,5 @@ urlpatterns = [
     path('tarefas/<slug:slug>/deletar/', views.deletar_lista, name='deletar-lista'),
     path('tarefas/<slug:slug>/tarefa/<int:tarefa_id>/deletar/', views.deletar_tarefa, name='deletar-tarefa'),
     path('tarefas/<slug:slug>/tarefa/<int:tarefa_id>/atualizar/', views.atualizar_tarefa, name='atualizar-tarefa'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
